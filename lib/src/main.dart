c56b42660323ffc48e04f25d1c18d24a9bb0b389
Jesther Jordan Minor <rejesthm@gmail.com>
2020-03-13 15:33:18 +0800

    setup pushnamed route on generate

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,5 +1,7 @@
+import 'package:anime_tambayan/src/errors/page_not_found.dart';
 import 'package:anime_tambayan/src/screens/authPage/login_page.dart';
 import 'package:flutter/material.dart';
+import 'routes/routers.dart' as routes;
 
 void main() => runApp(MyApp());
 
@@ -8,12 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginPage(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: LoginPage(),
+        onGenerateRoute: routes.generateRoute,
+        onUnknownRoute: (settings) => MaterialPageRoute(
+            builder: (context) => PageNotFoundPage(name: settings.name)));
   }
 }
-
